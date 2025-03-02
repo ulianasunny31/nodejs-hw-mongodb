ffc58e8c686ad0f1ca045d3a2725e0f9b10c663d
Uliana Seredenko <uliana1331@gmail.com>
2025-03-02 12:26:23 +0100

    add server err handling

--- src/server.js
+++ src/server.js
@@ -33,20 +33,27 @@ export function setupServer() {
 
   //get one by ids
   app.get('/contacts/:contactId', async (req, res) => {
-    const { contactId } = req.params;
-    const contact = await getContactById(contactId);
+    try {
+      const { contactId } = req.params;
+      const contact = await getContactById(contactId);
 
-    if (!contact) {
-      return res.status(404).json({
-        message: 'Contact not found',
+      if (!contact) {
+        return res.status(404).json({
+          message: 'Contact not found',
+        });
+      }
+
+      res.status(200).json({
+        status: 200,
+        message: `Successfully found contact with id ${contactId}!`,
+        data: contact,
+      });
+    } catch (error) {
+      return res.status(500).json({
+        message: 'Internal server error',
+        error: error.message,
       });
     }
-
-    res.status(200).json({
-      status: 200,
-      message: `Successfully found contact with id ${contactId}!`,
-      data: contact,
-    });
   });
 
   //non-existant routes handling
