442ed04ee15300b2fd7216ca9d7756ff2ba3ef1f
Uliana Seredenko <uliana1331@gmail.com>
2025-03-08 11:03:56 +0100

    correct next to throw error

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -47,8 +47,7 @@ export const deleteContactController = async (req, res, next) => {
   const contact = await deleteContact(contactId);
 
   if (!contact) {
-    next(createHttpError(404, 'Contact not found'));
-    return;
+    throw createHttpError(404, 'Contact not found');
   }
 
   res.status(204).send();
@@ -61,8 +60,7 @@ export const updateContactController = async (req, res, next) => {
   const result = await updateContact(contactId, req.body);
 
   if (!result) {
-    next(createHttpError(404, 'Contact not found'));
-    return;
+    throw createHttpError(404, 'Contact not found');
   }
 
   res.status(200).json({
