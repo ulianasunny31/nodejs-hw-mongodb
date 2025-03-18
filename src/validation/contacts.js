4bc2ffae24f7cab98fa51505c4d3dc5574425833
Uliana Seredenko <uliana1331@gmail.com>
2025-03-18 12:06:16 +0100

    make email and isFav not required when creating contact

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -2,10 +2,10 @@ import Joi from 'joi';
 
 export const createContactValidation = Joi.object({
   name: Joi.string().min(3).max(20).required(),
-  email: Joi.string().email().min(3).max(20).required(),
+  email: Joi.string().email().min(3).max(20),
   phoneNumber: Joi.string().min(8).max(15).required(),
   contactType: Joi.string().valid('personal', 'work', 'home').required(),
-  isFavourite: Joi.boolean().required(),
+  isFavourite: Joi.boolean(),
 });
 
 export const updateContactValidation = Joi.object({
