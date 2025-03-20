6d882d64e7eb13e2ffe7f237714984711489a880
Uliana Seredenko <uliana1331@gmail.com>
2025-03-20 13:24:16 +0100

    add users valid and db models

---
+++ src/db/model/users.js
@@ -0,0 +1,12 @@
+import { model, Schema } from 'mongoose';
+
+const UsersSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+  },
+  { timestamps: true, versionKey: false },
+);
+
+export const UsersCollection = model('users', UsersSchema);
