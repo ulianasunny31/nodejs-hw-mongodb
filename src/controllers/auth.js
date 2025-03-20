962c39dea78decd61d322017419b2b9aa56525e0
Uliana Seredenko <uliana1331@gmail.com>
2025-03-20 16:47:00 +0100

    revert

--- src/controllers/auth.js
+++
@@ -1,3 +0,0 @@
-export const registerUserController = async (req, res, next) => {
-  console.log(res.body);
-};
--- src/services/auth.js
+++
@@ -1,7 +0,0 @@
-import { UsersCollection } from '../db/model/users';
-
-export const registerUser = async (payload) => {
-  return await UsersCollection.create(payload);
-};
-
-export const loginUser = async () => {};
