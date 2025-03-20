43fa24d7f0f216b92c1e47468c8390d447b09624
Uliana Seredenko <uliana1331@gmail.com>
2025-03-20 17:25:24 +0100

    add check for non-unique email + encrypt password

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,7 +1,20 @@
+import createHttpError from 'http-errors';
 import { UsersCollection } from '../db/model/auth.js';
 
+import bcrypt from 'bcrypt';
+
 export const registerUser = async (payload) => {
-  return await UsersCollection.create(payload);
+  //hiding password
+  const encryptedPassword = await bcrypt.hash(payload.password, 10);
+
+  //checking for non-unique email
+  const existingUser = await UsersCollection.findOne({ email: payload.email });
+  if (existingUser) throw createHttpError(409, 'Email in use');
+
+  return await UsersCollection.create({
+    ...payload,
+    password: encryptedPassword,
+  });
 };
 
 export const loginUser = async () => {};
