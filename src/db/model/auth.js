8c898e3620a809cc8f86cb09566098ed9c1e5536
Uliana Seredenko <uliana1331@gmail.com>
2025-03-21 14:45:17 +0100

    hide password

--- src/db/model/auth.js
+++ src/db/model/auth.js
@@ -9,4 +9,10 @@ const UsersSchema = new Schema(
   { timestamps: true, versionKey: false },
 );
 
+UsersSchema.methods.toJSON = function () {
+  const obj = this.toObject();
+  delete obj.password;
+  return obj;
+};
+
 export const UsersCollection = model('users', UsersSchema);
